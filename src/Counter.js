efcdca2f1f059cdb3251b7706159fe672e71a6d4
aaaa <aaaa@aaa.aaa>
2022-12-16 15:18:01 +0100

    epic dziala

--- src/Counter.js
+++ src/Counter.js
@@ -2,14 +2,18 @@ import React, {Component} from "react";
 
 class Counter extends Component{
     state = {
-        count: 1
+        count: 1,
     }
+    //setState({ count: this.state.count + 1})
+    
+    //
+
 
     render(){
         return(
             <>
                 <span>{this.formatCount()}</span>
-                <button>increment</button>
+                <button onClick={()=>this.setState({ count: this.state.count + 1})} className="btn btn-primary" >increment</button>
             </>
         );
     }
