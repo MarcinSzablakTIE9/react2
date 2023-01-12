63beca5f9b7943e2e5f6fb3a97d4f2b1ea9fef54
aaaa <aaaa@aaa.aaa>
2023-01-12 15:14:54 +0100

    maslo

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getMovies } from "./fake_movies";
+import './movies.css'
 
 class Movies extends Component{
 
@@ -8,17 +9,28 @@ class Movies extends Component{
     }
 
     allMovies(){
-        return this.state.movies.map(movie => <p>{movie.title}</p>)
+        return this.state.movies.map(movie => <tr onClick={this.deleteMovies.bind(this, movie)}>
+            <td className='movieId'>{movie._id}</td>
+            <td>{movie.title}</td>
+            <td>{movie.genre._id}</td>
+            <td>{movie.genre.name}</td>
+            <td>{movie.numberInStock}</td>
+            <td>{movie.dailyRentalRate}</td>
+            </tr>
+            )
+    }
+    deleteMovies(movie){
+        const movies = this.state.movies.filter(i => i._id !== movie._id)
+        this.setState({movies})
     }
 
     //Show movies from {this.state.movies[0].title}
 
     render(){
-
         return (
-            <>
-                <p>{this.allMovies()}</p>
-            </>
+            <table>
+                <>{this.allMovies()}</>
+            </table>
         )
     }
 }
